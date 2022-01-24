6951e0cec3031d68733dec91fa9380bd8bf1176a
jayylmao <73204320+jayylmao@users.noreply.github.com>
2022-01-24 15:40:12 +0800

    Put constraints for certain elements

--- main.js
+++ main.js
@@ -1,53 +1,88 @@
-document.addEventListener('scroll', function(){
+document.addEventListener('scroll', function () {
     var h = document.documentElement, 
         b = document.body,
         st = 'scrollTop',
         sh = 'scrollHeight';
 
     var percent = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
-    
+    var adjPercent = percent * 2.5 - 150;
+
     getComputedStyle(document.documentElement);
 
-    if (percent <= 25) {
-        document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(100, 100, 180)';
-        document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(80, 80, 90)';
-        document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(80, 80, 90)';
-        document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(80, 80, 90)';
+    var darkMode = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // checks the percentage scrolled and modifies the sidebar text accordingly
+    // if (darkMode.matches) checks the css media query and adjusts the colors
+    if (percent <= 15) {
+        if (darkMode.matches) {
+            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(135, 135, 190)';
+            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(105, 105, 125)';
+            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(105, 105, 125)';
+            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(105, 105, 125)';
+        } else {
+            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(100, 100, 180)';
+            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(80, 80, 90)';
+            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(80, 80, 90)';
+            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(80, 80, 90)';
+        }
 
         document.getElementsByClassName("pageLabel")[0].innerHTML = '· // WELCOME';
         document.getElementsByClassName("pageLabel")[1].innerHTML = '// MGA DOKUMENTO';
         document.getElementsByClassName("pageLabel")[2].innerHTML = '// REPLEKSYON';
         document.getElementsByClassName("pageLabel")[3].innerHTML = '// BIONOTE';
-    } else if (percent >= 25 && percent <= 50) {
-        document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(80, 80, 90)';
-        document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(100, 100, 180)';
-        document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(80, 80, 90)';
-        document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(80, 80, 90)';
+    } else if (percent >= 15 && percent <= 50) {
+        if (darkMode.matches) {
+            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(105, 105, 125)';
+            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(135, 135, 190)';
+            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(105, 105, 125)';
+            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(105, 105, 125)';
+        } else {
+            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(80, 80, 90)';
+            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(100, 100, 180)';
+            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(80, 80, 90)';
+            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(80, 80, 90)';   
+        }
 
         document.getElementsByClassName("pageLabel")[0].innerHTML = '// WELCOME';
         document.getElementsByClassName("pageLabel")[1].innerHTML = '· // MGA DOKUMENTO';
         document.getElementsByClassName("pageLabel")[2].innerHTML = '// REPLEKSYON';
         document.getElementsByClassName("pageLabel")[3].innerHTML = '// BIONOTE';
     } else if (percent >= 50 && percent <= 75) {
-        document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(80, 80, 90)';
-        document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(80, 80, 90)';
-        document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(100, 100, 180)';
-        document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(80, 80, 90)';
+        if (darkMode.matches) {
+            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(105, 105, 125)';
+            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(105, 105, 125)';
+            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(135, 135, 190)';
+            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(105, 105, 125)';
+        } else {
+            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(80, 80, 90)';
+            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(80, 80, 90)';
+            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(100, 100, 180)';
+            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(80, 80, 90)';   
+        }
 
         document.getElementsByClassName("pageLabel")[0].innerHTML = '// WELCOME';
         document.getElementsByClassName("pageLabel")[1].innerHTML = '// MGA DOKUMENTO';
         document.getElementsByClassName("pageLabel")[2].innerHTML = '· // REPLEKSYON';
         document.getElementsByClassName("pageLabel")[3].innerHTML = '// BIONOTE';
     } else {
-        document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(80, 80, 90)';
-        document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(80, 80, 90)';
-        document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(80, 80, 90)';
-        document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(100, 100, 180)';
+        if (darkMode.matches) {
+            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(105, 105, 125)';
+            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(105, 105, 125)';
+            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(105, 105, 125)';
+            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(135, 135, 190)';
+        } else {
+            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(80, 80, 90)';
+            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(80, 80, 90)';
+            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(80, 80, 90)';
+            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(100, 100, 180)';
+        }
         
         document.getElementsByClassName("pageLabel")[0].innerHTML = '// WELCOME';
         document.getElementsByClassName("pageLabel")[1].innerHTML = '// MGA DOKUMENTO';
         document.getElementsByClassName("pageLabel")[2].innerHTML = '// REPLEKSYON';
         document.getElementsByClassName("pageLabel")[3].innerHTML = '· // BIONOTE';
     }
+
+    document.getElementById('sidebarContainer').style.marginBottom = adjPercent + 'px';
     console.log("scrolled: " + percent);
 });
