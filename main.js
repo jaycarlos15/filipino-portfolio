43809fffa13202b37227ea1c07a7893ba57470bb
jayylmao <73204320+jayylmao@users.noreply.github.com>
2022-01-26 12:06:47 +0800

    added mobile navbar animation

--- main.js
+++ main.js
@@ -4,24 +4,27 @@
 
 
 var darkMode = window.matchMedia("(prefers-color-scheme: dark)");
+var sunImage = document.getElementById('sunImage');
+var link = document.getElementsByTagName('link');
+var tooltip = document.getElementById('dmTooltip');
 var pageIsDark;
 
 if (darkMode.matches) {
     // if system is dark, turn pageIsDark to true and move the sun image down
     pageIsDark = true;
-    document.getElementById('sunImage').style.marginTop = '0';
+    sunImage.style.marginTop = '0';
     // enable dark.css and disable light.css
-    document.getElementsByTagName('link')[1].disabled = false;
-    document.getElementsByTagName('link')[2].disabled = true;
-    document.getElementById('dmTooltip').innerHTML = 'LIGHT MODE';
+    link[1].disabled = false;
+    link[2].disabled = true;
+    tooltip.innerHTML = 'LIGHT MODE';
 } else {
     // if system is light, turn pageIsDark to false and move the sun image up
     pageIsDark = false;
-    document.getElementById('sunImage').style.marginTop = '-1em';
+    sunImage.style.marginTop = '-1em';
     // disable dark.css and enable light.css
-    document.getElementsByTagName('link')[1].disabled = true;
-    document.getElementsByTagName('link')[2].disabled = false;
-    document.getElementById('dmTooltip').innerHTML = 'DARK MODE';
+    link[1].disabled = true;
+    link[2].disabled = false;
+    tooltip.innerHTML = 'DARK MODE';
 }
 
 function darkModeToggle() {
@@ -58,78 +61,78 @@ document.addEventListener('scroll', function () {
 
     getComputedStyle(document.documentElement);
 
-    var pageLabel = 'document.getElementsByClassName("pageLabel")';
+    var pageLabel = document.getElementsByClassName("pageLabel");
 
     // checks the percentage scrolled and modifies the sidebar text accordingly
     // if (darkMode.matches) checks the css media query and adjusts the colors
     if (percent <= 25) {
         if (pageIsDark == true) {
-            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(135, 135, 190)';
-            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(105, 105, 125)';
-            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(105, 105, 125)';
-            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(105, 105, 125)';
+            pageLabel[0].style.color = 'rgb(135, 135, 190)';
+            pageLabel[1].style.color = 'rgb(105, 105, 125)';
+            pageLabel[2].style.color = 'rgb(105, 105, 125)';
+            pageLabel[3].style.color = 'rgb(105, 105, 125)';
         } else {
-            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(100, 100, 180)';
-            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(120, 120, 140)';
-            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(120, 120, 140)';
-            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(120, 120, 140';
+            pageLabel[0].style.color = 'rgb(100, 100, 180)';
+            pageLabel[1].style.color = 'rgb(120, 120, 140)';
+            pageLabel[2].style.color = 'rgb(120, 120, 140)';
+            pageLabel[3].style.color = 'rgb(120, 120, 140';
         }
 
-        document.getElementsByClassName("pageLabel")[0].innerHTML = '· // WELCOME';
-        document.getElementsByClassName("pageLabel")[1].innerHTML = '// BIONOTE';
-        document.getElementsByClassName("pageLabel")[2].innerHTML = '// MGA DOKUMENTO';
-        document.getElementsByClassName("pageLabel")[3].innerHTML = '// REPLEKSYON';
+        pageLabel[0].innerHTML = '· // WELCOME';
+        pageLabel[1].innerHTML = '// BIONOTE';
+        pageLabel[2].innerHTML = '// MGA DOKUMENTO';
+        pageLabel[3].innerHTML = '// REPLEKSYON';
     } else if (percent >= 25 && percent <= 45) {
         if (pageIsDark == true) {
-            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(105, 105, 125)';
-            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(135, 135, 190)';
-            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(105, 105, 125)';
-            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(105, 105, 125)';
+            pageLabel[0].style.color = 'rgb(105, 105, 125)';
+            pageLabel[1].style.color = 'rgb(135, 135, 190)';
+            pageLabel[2].style.color = 'rgb(105, 105, 125)';
+            pageLabel[3].style.color = 'rgb(105, 105, 125)';
         } else {
-            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(120, 120, 140)';
-            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(100, 100, 180)';
-            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(120, 120, 140)';
-            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(120, 120, 140)';   
+            pageLabel[0].style.color = 'rgb(120, 120, 140)';
+            pageLabel[1].style.color = 'rgb(100, 100, 180)';
+            pageLabel[2].style.color = 'rgb(120, 120, 140)';
+            pageLabel[3].style.color = 'rgb(120, 120, 140)';   
         }
 
-        document.getElementsByClassName("pageLabel")[0].innerHTML = '// WELCOME';
-        document.getElementsByClassName("pageLabel")[1].innerHTML = '· // BIONOTE';
-        document.getElementsByClassName("pageLabel")[2].innerHTML = '// MGA DOKUMENTO';
-        document.getElementsByClassName("pageLabel")[3].innerHTML = '// REPLEKSYON';
+        pageLabel[0].innerHTML = '// WELCOME';
+        pageLabel[1].innerHTML = '· // BIONOTE';
+        pageLabel[2].innerHTML = '// MGA DOKUMENTO';
+        pageLabel[3].innerHTML = '// REPLEKSYON';
     } else if (percent >= 45 && percent <= 75) {
         if (pageIsDark == true) {
-            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(105, 105, 125)';
-            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(105, 105, 125)';
-            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(135, 135, 190)';
-            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(105, 105, 125)';
+            pageLabel[0].style.color = 'rgb(105, 105, 125)';
+            pageLabel[1].style.color = 'rgb(105, 105, 125)';
+            pageLabel[2].style.color = 'rgb(135, 135, 190)';
+            pageLabel[3].style.color = 'rgb(105, 105, 125)';
         } else {
-            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(120, 120, 140)';
-            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(120, 120, 140)';
-            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(100, 100, 180)';
-            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(120, 120, 140)';   
+            pageLabel[0].style.color = 'rgb(120, 120, 140)';
+            pageLabel[1].style.color = 'rgb(120, 120, 140)';
+            pageLabel[2].style.color = 'rgb(100, 100, 180)';
+            pageLabel[3].style.color = 'rgb(120, 120, 140)';   
         }
 
-        document.getElementsByClassName("pageLabel")[0].innerHTML = '// WELCOME';
-        document.getElementsByClassName("pageLabel")[1].innerHTML = '// BIONOTE';
-        document.getElementsByClassName("pageLabel")[2].innerHTML = '· // MGA DOKUMENTO';
-        document.getElementsByClassName("pageLabel")[3].innerHTML = '// REPLEKSYON';
+        pageLabel[0].innerHTML = '// WELCOME';
+        pageLabel[1].innerHTML = '// BIONOTE';
+        pageLabel[2].innerHTML = '· // MGA DOKUMENTO';
+        pageLabel[3].innerHTML = '// REPLEKSYON';
     } else {
         if (pageIsDark == true) {
-            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(105, 105, 125)';
-            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(105, 105, 125)';
-            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(105, 105, 125)';
-            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(135, 135, 190)';
+            pageLabel[0].style.color = 'rgb(105, 105, 125)';
+            pageLabel[1].style.color = 'rgb(105, 105, 125)';
+            pageLabel[2].style.color = 'rgb(105, 105, 125)';
+            pageLabel[3].style.color = 'rgb(135, 135, 190)';
         } else {
-            document.getElementsByClassName("pageLabel")[0].style.color = 'rgb(120, 120, 140)';
-            document.getElementsByClassName("pageLabel")[1].style.color = 'rgb(120, 120, 140)';
-            document.getElementsByClassName("pageLabel")[2].style.color = 'rgb(120, 120, 140)';
-            document.getElementsByClassName("pageLabel")[3].style.color = 'rgb(100, 100, 180)';
+            pageLabel[0].style.color = 'rgb(120, 120, 140)';
+            pageLabel[1].style.color = 'rgb(120, 120, 140)';
+            pageLabel[2].style.color = 'rgb(120, 120, 140)';
+            pageLabel[3].style.color = 'rgb(100, 100, 180)';
         }
         
-        document.getElementsByClassName("pageLabel")[0].innerHTML = '// WELCOME';
-        document.getElementsByClassName("pageLabel")[1].innerHTML = '// BIONOTE';
-        document.getElementsByClassName("pageLabel")[2].innerHTML = '// MGA DOKUMENTO';
-        document.getElementsByClassName("pageLabel")[3].innerHTML = '· // REPLEKSYON';
+        pageLabel[0].innerHTML = '// WELCOME';
+        pageLabel[1].innerHTML = '// BIONOTE';
+        pageLabel[2].innerHTML = '// MGA DOKUMENTO';
+        pageLabel[3].innerHTML = '· // REPLEKSYON';
     }
 
     document.getElementById('sidebarContainer').style.marginBottom = adjPercent + 'px';
@@ -164,6 +167,7 @@ document.addEventListener('scroll', function () {
 
 var sidebarVisible = false;
 var sidebar = document.getElementById('sidebar');
+var close = document.getElementById('close');
 var desktopWidth = window.matchMedia("(min-width: 601px)");
 
 desktopWidth.addEventListener('change', function () {
@@ -180,10 +184,12 @@ function sidebarToggle() {
         sidebarVisible = true;
         sidebar.style.opacity = 1;
         sidebar.style.zIndex = 1;
+        close.style.marginTop = 0;
     } else {
         sidebarVisible = false;
         sidebar.style.opacity = 0;
         sidebar.style.zIndex = -1;
+        close.style.marginTop = '-2em';
     }
 }
 
